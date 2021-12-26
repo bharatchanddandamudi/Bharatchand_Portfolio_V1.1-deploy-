3a4e5f6dc822b5c46a84ba8b0df13df927f88a1d
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2021-12-26 13:16:31 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z6+xb+i07xlnu$&ik9rs)e%q*gdkf2voyr(-_y=l(k%z-0fd0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  
+DEBUG = Ture  
 
-ALLOWED_HOSTS = ['127.0.0.1','bharatchand-2-portfolio-git-bharatchand01-dev.apps.sandbox-m2.ll9k.p1.openshiftapps']
+ALLOWED_HOSTS = ['127.0.0.1','bharatchand-2-bharatchand01-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com']
 
 
 # Application definition
