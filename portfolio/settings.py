dda6ce7ffc827fea256e93b68aab1b391275fe5d
Bharatchand Dandamudi <bharatchand.tree@gmail.com>
2021-12-26 15:22:00 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'portfoliodb',
-#         'USER':'bbc',
-#         'PASSWORD':'bbc1',
-#         'HOST': 'localhost',
-#         'PORT':'5432',
-#     }	
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'bbc',
+        'PASSWORD':'bbc1',
+        'HOST': 'localhost',
+        'PORT':'5432',
+    }	
+}
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.sqlite3',
